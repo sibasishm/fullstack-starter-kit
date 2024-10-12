eafd62102a901a5426dd24ccc83b8b895666c1e3
Sibasish Mohanty <smsibasish@gmail.com>
2024-10-12 17:47:06 +0530

    app setup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import app from './app';
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const port = 3000;
+console.log(`Server is running on port http://localhost:${port}`);
 
 serve({
-  fetch: app.fetch,
-  port
-})
+	fetch: app.fetch,
+	port,
+});
