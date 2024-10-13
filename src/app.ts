81631036c55f58b92294dc4949414aed4bb60cf2
Sibasish Mohanty <smsibasish@gmail.com>
2024-10-13 11:30:55 +0530

    setup scalar and openapi

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
-import createApp from '@/lib/create-app';
+import configureOpenAPI from "@/lib/configure-open-api";
+import createApp from "@/lib/create-app";
+import index from "@/routes/index.route";
 
 const app = createApp();
 
-app.get('/', c => {
-	return c.text('Hello Hono!');
+configureOpenAPI(app);
+
+const routes = [
+	index,
+] as const;
+
+routes.forEach((route) => {
+	app.route("/", route);
 });
+
+export type AppType = typeof routes[number];
 
 export default app;
